540a132067aec34c157a5e772a00a97eccaf2092
maklumi <maklumi@yahoo.com>
2020-05-08 21:36:05 +0800

    single product component

--- src/komponen/Produk/Product.svelte
+++ src/komponen/Produk/Product.svelte
@@ -1,1 +1,18 @@
-<h1>satu produk</h1>
+<script>
+  export let produk;
+  const { title, image, price, id } = produk;
+  import { link } from "svelte-routing";
+</script>
+
+<article class="product">
+  <div class="img-container">
+    <img src={image} alt="title" />
+    <a href={`/produk/${id}`} class="btn btn-primary product-link" use:link>
+      Lanjut
+    </a>
+  </div>
+  <div class="product-footer">
+    <p class="product-title">{title}</p>
+    <p class="product-title">${price}</p>
+  </div>
+</article>
