95fcf3f5ce3310cb40df960e1f19df90251c4130
maklumi <maklumi@yahoo.com>
2020-05-08 16:24:53 +0800

    subscribe dan unsubscribe dengan $ sign

--- src/komponen/Produk/Products.svelte
+++ src/komponen/Produk/Products.svelte
@@ -1,17 +1,9 @@
 <script>
   import { onMount, onDestroy } from "svelte";
   import barangan from "../../stor/defaultProduk";
-
-  let localProducts = [];
-  onMount(() => {
-    barangan.subscribe((nilai) => {
-      localProducts = nilai;
-      console.log(nilai);
-    });
-  });
 </script>
 
 <h1>products comp</h1>
-{#each localProducts as produk}
+{#each $barangan as produk}
   <h2>{produk.title}</h2>
 {/each}
