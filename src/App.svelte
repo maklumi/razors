b589a449d6e1bc426698acf66dec9a216ff0a67b
maklumi <maklumi@yahoo.com>
2020-05-08 10:14:58 +0800

    project structures

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,16 @@
 <script>
-
+  // pages
+  import About from "./laman/About.svelte";
+  import Cekout from "./laman/Cekout.svelte";
+  import Home from "./laman/Home.svelte";
+  import Login from "./laman/Login.svelte";
+  import Products from "./laman/Products.svelte";
+  import ProductTemplate from "./laman/ProductTemplate.svelte";
 </script>
 
 <style>
 
 </style>
 
-<h1>Hello!</h1>
+<About />
+<i class="fas fa-home fa-7x" />
---
+++ src/laman/ProductTemplate.svelte
@@ -0,0 +1,1 @@
+<h1>Produk template</h1>
