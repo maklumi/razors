5ed9d84505ccfd2b64768b3c1dc3a06c19dba743
maklumi <maklumi@yahoo.com>
2020-05-09 10:49:22 +0800

    item lis transition

--- src/komponen/Troli/LisItem.svelte
+++ src/komponen/Troli/LisItem.svelte
@@ -8,7 +8,12 @@
 <section class="cart-items">
   <article>
     {#each $dalamTroli as item, indeks (item.id)}
-      <Item {item} />
+      <div
+        in:fly={{ delay: (indeks + 1) * 400, x: 100 }}
+        out:fly={{ x: -100 }}
+        animate:flip>
+        <Item {item} />
+      </div>
     {:else}
       <h2 class="empty-cart">Troli masih kosong</h2>
     {/each}
