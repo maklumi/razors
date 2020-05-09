ecb39a8e0ea2de05b9bddc4dd9302a0ec7ca82df
maklumi <maklumi@yahoo.com>
2020-05-09 18:47:24 +0800

    setup STRAPI

---
+++ src/strapi/getProduks.js
@@ -0,0 +1,13 @@
+import url from './URL'
+
+export default async () => {
+  const jsonresponse = await fetch(`${url}/produks`)
+    .then()
+    .catch((err) => console.error(err))
+
+  const produks = await jsonresponse.json()
+  if (produks.error) {
+    return null
+  }
+  return produks
+}
