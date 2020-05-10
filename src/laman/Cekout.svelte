9c148a2e6df4a88a5b8bc9a4e6aee762e2032776
maklumi <maklumi@yahoo.com>
2020-05-11 06:56:48 +0800

    prevent cekout access thru address bar

--- src/laman/Cekout.svelte
+++ src/laman/Cekout.svelte
@@ -3,6 +3,12 @@
   import { navigate, link } from 'svelte-routing'
   import userstor from '../stor/user'
   import { jumlahHarga } from '../stor/troli'
+
+  onMount(() => {
+    if (!$userstor.jwt) {
+      navigate('/')
+    }
+  })
 </script>
 
 <h1>Cekout</h1>
